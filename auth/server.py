d26609461b42938d5ce1b47fc882f8110ac4ab47
aymanfouad123 <aymanfouad033@gmail.com>
2025-11-10 16:18:10 -0500

    feat: validate jwt (basic)

--- auth/server.py
+++ auth/server.py
@@ -38,21 +38,39 @@ def login():
     if auth.username != email or auth.password != password:
         return jsonify({"error": "invalid credentials"}), 401
 
-    token = createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    token = createJWT(auth.username, os.getenv("JWT_SECRET"), True)
     return jsonify({"token": token}), 200
 
 def createJWT(username, secret, authz):
     return jwt.encode(
         {
             "username": username,
-            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
-            + datetime.timedelta(days=1),
-            "iat": datetime.datetime.utcnow(),
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1), # 1 day expiration
+            "iat": datetime.datetime.now(tz=datetime.timezone.utc), # issued at
             "admin": authz,
         },
         secret,
         algorithm="HS256",
     )
+    
+@app.route("/validate", methods=["POST"])
+def validate():
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header:
+        return jsonify({"error": "missing credentials"}), 401
+
+    try:
+        encoded_jwt = auth_header.split(" ")[1]
+        decoded = jwt.decode(
+            encoded_jwt, os.getenv("JWT_SECRET"), algorithms=["HS256"]
+        )
+        return jsonify(decoded), 200
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "token expired"}), 403
+    except jwt.InvalidTokenError:
+        return jsonify({"error": "not authorized"}), 403
 
 
 if __name__ == "__main__":
